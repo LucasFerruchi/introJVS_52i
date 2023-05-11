28f80046454a9e0e9b5fde5dfeb1b50a8786e741
LucasFerruchi <lucasferruchi05@gmail.com>
2023-05-11 17:39:55 -0300

    comenzamos nuestro primer codigo javaScrpit

---
+++ js/a_primeraClase.js
@@ -0,0 +1,39 @@
+//Comentorios
+
+//comentario de una sola linea
+
+/*Comentorio de varias lineas*/
+
+//Como ejecutar JVS
+//Directo en HTML
+//Consola
+//Creando archivo JVS
+
+//imprimir por consola
+console.log("Hola"); //comentario
+
+// console.error("Soy un error");
+console.warn("Soy una alerta");
+
+document.write("Hola");
+
+//SINTAXIS
+
+//Let
+//camelCase
+let nombreApellido = "Lucas Ferruchi";
+/*Las variables let pueden reasignarse*/
+nombreApellido="Juan Carlos";
+console.log(nombreApellido);
+
+
+//Var
+
+var alumno = "Luci";
+console.log(alumno);
+
+//Const
+
+const PI=3.14;
+console.log(PI);
+
