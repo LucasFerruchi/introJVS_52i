1b6cd5b226ea983c8df0d0369a1ea57e1d625b5a
LucasFerruchi <lucasferruchi05@gmail.com>
2023-05-16 17:20:24 -0300

    Variables, Tipos de datos y Operadores

--- js/a_primeraClase.js
+++ js/a_primeraClase.js
@@ -1,39 +1,155 @@
-//Comentorios
+// //Comentorios
 
-//comentario de una sola linea
+// //comentario de una sola linea
 
-/*Comentorio de varias lineas*/
+// /*Comentorio de varias lineas*/
 
-//Como ejecutar JVS
-//Directo en HTML
-//Consola
-//Creando archivo JVS
+// //Como ejecutar JVS
+// //Directo en HTML
+// //Consola
+// //Creando archivo JVS
 
-//imprimir por consola
-console.log("Hola"); //comentario
+// //imprimir por consola
+// console.log("Hola"); //comentario
 
 // console.error("Soy un error");
-console.warn("Soy una alerta");
+// console.warn("Soy una alerta");
 
-document.write("Hola");
+// document.write("Hola");
 
-//SINTAXIS
+// //SINTAXIS
 
-//Let
-//camelCase
-let nombreApellido = "Lucas Ferruchi";
-/*Las variables let pueden reasignarse*/
-nombreApellido="Juan Carlos";
-console.log(nombreApellido);
+// //Let
+// //camelCase
+// let nombreApellido = "Lucas Ferruchi";
+// console.log(nombreApellido);
+// // let nombreApellido =
+// /*Las variables let pueden reasignarse*/
+// nombreApellido = "Juan Carlos";
+// console.log(nombreApellido);
 
+// //Var
 
-//Var
+// var alumno = "Luci";
 
-var alumno = "Luci";
-console.log(alumno);
+// // var alumno = "Carlos";
+// console.log(alumno);
 
-//Const
+// //Const
 
-const PI=3.14;
-console.log(PI);
+// const PI = 3.14;
+// console.log(PI);
+// // PI = 34;
 
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// //OPERADORES
+
+// //Asignacion
+// let caja = true;
+
+// //Aritmeticos
+// // let numero1 = 987;
+// // let numero2 = 34;
+// // let resultado = numero1 % numero2;
+// // console.log(resultado);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// //OJO con el + en string
+
+// let nombre = "Lucas";
+// let apellido = "Ferruchi";
+// let edad = 38;
+
+// console.log(nombre + " " + apellido + " " + edad);
+
+// //En consola, aparezc el siguiente mensaje
+// //Nombre: Lucas Apellio: Ferruchi Edad: 38
+
+// console.log("Nombre: " + nombre + " Apellido: " + apellido + " Edad: " + edad);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// //OPERADORES DE RELACION O COMPARACION
+
+// let numero1 = 34;
+// let numero2 = "34";
+
+// console.log(numero1 === numero2);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+//OPERADORES UNITARIOS
+// ++ y --
+
+// ++numero1;
+// --numero1;
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+//OPERADORES LOGICOS
+//&&
+//||
+
+// console.log(2 < 3 || 3 > 4);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// //OPERADOR DE NEGACION
+// // "!"
+// //Invierte el valor
+
+// let caja = true;
+// console.log(!caja);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// //NULL
+
+// let numero = 15;
+
+// let billetera = null;
+
+// console.log(numero / billetera);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// //UNDEFINED
+
+// let producto = "";
+
+// let numero = 15;
+// console.log(producto / numero);
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+//ARRAYS O ARREGLOS
+
+let caja = [12, 65, -65, 67.34];
+
+let ficha = ["Carlos", "Camila", "Luci", "Juan"];
+
+let ficha1 = ["Lucas", "Jorge", "sofia", 540, [32, true]];
+
+//OBJETOS
+
+let persona = {
+  nombre: "Lucas",
+  apellido: "Ferruchi",
+  edad: 38,
+  cursando: true,
+  domicilio: ["Corrientes ", 78, " San Miguel"],
+};
+
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
